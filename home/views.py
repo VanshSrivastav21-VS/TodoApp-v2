f88f147ec66c7f156ccdef97c79866d8078a471e
VanshSrivastav <vanshsrivastav2004@gmail.com>
2023-11-21 18:38:13 +0530

    first commit

---
+++ home/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from .models import Task
+
+# Create your views here.
+def index(request):
+    context = {
+        'task_list' : Task.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def add_task(request):
+    if request.method == 'POST':
+        task = request.POST.get('task')
+        if len(task) >= 2:
+            Task.objects.create(title=task)
+    redirect('home')
+
+def update_task(request, id):
+    context = {}
+    return render(request, 'todo_list.html', context)
+
+def delete_task(request, id):
+    pass
+    # context = {}
+    # return render(request, 'todo_list.html', context)
+
